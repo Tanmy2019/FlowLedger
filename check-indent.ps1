0a091c0fa88e6a8795d447b39b985c7774ec6f15
Tanmy2019 <Foxmailjp@163.com>
2026-05-22 15:44:27 +0800

    1

--- check-indent.ps1
+++
@@ -1,9 +0,0 @@
-$f = 'e:\Code_Project\FlowLedger\src\app\dashboard\accounts\page.tsx'
-$lines = [System.IO.File]::ReadAllLines($f)
-Write-Host "Line 152 raw chars:"
-$chars = $lines[151].ToCharArray()
-for ($i = 0; $i -lt 10 -and $i -lt $chars.Length; $i++) {
-    if ($chars[$i] -eq 9) { Write-Host "  index $i = TAB" }
-    elseif ($chars[$i] -eq 32) { Write-Host "  index $i = SPACE" }
-    else { Write-Host "  index $i = '$($chars[$i])'" }
-}
